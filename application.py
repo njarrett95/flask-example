25714ff5408a6abc41e9d42666e8e35f6a68750f
Nathaniel J <njarrett95@gmail.com>
2022-09-27 23:24:11 -0500

    Adding HTML

--- application.py
+++ application.py
@@ -1,20 +1,27 @@
 from flask import Flask
+from flask import request
+
 application = Flask(__name__)
 app = application
 
 @app.route('/')     # decorator assigning route for index() function
 def index():
-    return 'Hello there. From Python.'
+    celsius = request.args.get("celsius", "")
+    return (
+        """<form action="" method="get">
+                <input type="text" name="celsius">
+                <input type="submit" value="Convert">
+              </form>"""
+        + celsius
+    )
 
 @app.route('/<int:celsius>')     # must have different route or FIFO takes place
+                                 # can check for data type and pass into function with <>
 def fahrenheit_from(celsius):
     """Convert Celsius to Fahrenheit degrees."""
-    try:
-        fahrenheit = float(celsius) * 9 / 5 + 32
-        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
-        return str(fahrenheit)
-    except ValueError:
-        return "invalid input"
+    fahrenheit = float(celsius) * 9 / 5 + 32
+    fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+    return str(fahrenheit)
 
 if __name__ == "__main__":
     celsius = input("Celsius: ")
