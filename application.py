13baeb7c58aa327baf3a09278fb5cb389d0522c4
Nathaniel J <njarrett95@gmail.com>
2022-09-27 23:33:35 -0500

    HTML for user submitted celsius

--- application.py
+++ application.py
@@ -7,21 +7,29 @@ app = application
 @app.route('/')     # decorator assigning route for index() function
 def index():
     celsius = request.args.get("celsius", "")
+    if celsius:
+        farhenheit = fahrenheit_from(celsius)
+    else:
+        fahrenheit = ""
     return (
         """<form action="" method="get">
                 <input type="text" name="celsius">
                 <input type="submit" value="Convert">
               </form>"""
-        + celsius
+        + "Fahrenheit: "
+        + fahrenheit
     )
 
-@app.route('/<int:celsius>')     # must have different route or FIFO takes place
+# @app.route('/<int:celsius>')     # must have different route or FIFO takes place
                                  # can check for data type and pass into function with <>
 def fahrenheit_from(celsius):
     """Convert Celsius to Fahrenheit degrees."""
-    fahrenheit = float(celsius) * 9 / 5 + 32
-    fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
-    return str(fahrenheit)
+    try:
+        fahrenheit = float(celsius) * 9 / 5 + 32
+        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+        return str(fahrenheit)
+    except ValueError:
+        return "invalid input"
 
 if __name__ == "__main__":
     celsius = input("Celsius: ")
