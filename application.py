253b5972c4f97a5510b08d5819ceb60c629801c2
Nathaniel J <njarrett95@gmail.com>
2022-09-27 23:01:53 -0500

    Adding celsius - fahrenheit converter

--- application.py
+++ application.py
@@ -1,6 +1,21 @@
 from flask import Flask
 application = Flask(__name__)
+app = application
 
-@application.route('/')
-def hello_world():
-    return 'Hello there. From Python.' 
+@app.route('/')     # decorator assigning route for index() function
+def index():
+    return 'Hello there. From Python.'
+
+@app.route('/<celsius>')     # must have different route or FIFO takes place
+def fahrenheit_from(celsius):
+    """Convert Celsius to Fahrenheit degrees."""
+    try:
+        fahrenheit = float(celsius) * 9 / 5 + 32
+        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+        return str(fahrenheit)
+    except ValueError:
+        return "invalid input"
+
+if __name__ == "__main__":
+    celsius = input("Celsius: ")
+    print("Fahrenheit:", fahrenheit_from(celsius))
